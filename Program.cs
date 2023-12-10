c5b90f5af44f506f3495ef43157f05984529c1f8
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2023-12-10 16:58:21 -0300

    :rocket: Enviando Arquivos!!

--- Program.cs
+++ Program.cs
@@ -33,3 +33,30 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+// para rodar o dotnet 6.0 com mysql, via docker, é necessário criar um arquivo Dockerfile
+/* e adicionar o seguinte conteúdo:
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /source
+
+# copy csproj and restore as distinct layers
+COPY *.sln .
+COPY appcsharp/*.csproj ./appcsharp/
+RUN dotnet restore
+
+# copy everything else and build app
+COPY appcsharp/. ./appcsharp/
+WORKDIR /source/appcsharp
+RUN dotnet publish -c release -o /app --no-restore
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+WORKDIR /app
+COPY --from=build /app ./
+ENTRYPOINT ["dotnet", "appcsharp.dll"]
+
+e rodar o seguinte comando:
+docker build -t appcsharp .
+docker run -p 8080:80 appcsharp
+
+*/
